527eccfbdbfee4baa691a5794f728c2bf6272394
Eliseu Videira <eliseuvideira1@gmail.com>
2019-10-13 14:18:38 -0300

    Part II: The xunit example. Chapter 18: First steps to xunit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,49 @@
-console.log('Hello World!');
+/* TODO
+ **  [X] Invoke test method
+ **  Invoke setUp first
+ **  Invoke tearDown afterward
+ **  Invoke tearDown even if the test method fails
+ **  Run multiple tests
+ **  Report collected results
+ */
+class TestCase {
+  public name: string;
+  constructor(name) {
+    this.name = name;
+  }
+
+  run(): void {
+    this[this.name]();
+  }
+}
+
+class WasRun extends TestCase {
+  public wasRun: boolean;
+
+  constructor(name) {
+    super(name);
+    this.wasRun = false;
+  }
+
+  testMethod(): void {
+    this.wasRun = true;
+  }
+}
+
+class TestCaseTest extends TestCase {
+  assert(value: boolean): void {
+    if (value) {
+      return console.log('OK');
+    }
+    throw new Error('Expected true, got false');
+  }
+
+  testRunning(): void {
+    const test = new WasRun('testMethod');
+    this.assert(!test.wasRun);
+    test.run();
+    this.assert(test.wasRun);
+  }
+}
+
+new TestCaseTest('testRunning').run();
